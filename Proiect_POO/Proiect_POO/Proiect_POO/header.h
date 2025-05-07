22d989e028376c5dcb7898c92daaa31e2f46af0c
Bighiu Rares <bighiurares05@gmail.com>
2025-05-07 21:04:07 +0300

    Add files via upload

---
+++ Proiect_POO/Proiect_POO/Proiect_POO/header.h
@@ -0,0 +1,121 @@
+﻿#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+
+using namespace std;
+
+class Task {
+protected:
+    string nume;
+public:
+    Task(string n) : nume(n) {}
+    virtual void afisare() const = 0;
+    virtual ~Task() {}
+};
+
+class TaskCuTermen : public Task {
+    string termen;
+public:
+    TaskCuTermen(string n, string t) : Task(n), termen(t) {}
+    void afisare() const override {
+        cout << "Task: " << nume << ", Termen: " << termen << endl;
+    }
+};
+
+class TaskCuBuget : public Task {
+    double buget;
+public:
+    TaskCuBuget(string n, double b) : Task(n), buget(b) {}
+    void afisare() const override {
+        cout << "Task: " << nume << ", Buget: " << buget << endl;
+    }
+};
+
+class TaskComplet : public Task {
+    double buget;
+    string termen;
+public:
+    TaskComplet(string n, double b, string t) : Task(n), buget(b), termen(t) {}
+    void afisare() const override {
+        cout << "Task: " << nume << ", Buget: " << buget << ", Termen: " << termen << endl;
+    }
+};
+
+class Angajat {
+protected:
+    string nume;
+    double salariu;
+    string functie;
+    string departament;
+    vector<shared_ptr<Task>> taskuri;
+
+public:
+    Angajat(string n, double s, string f, string d) :
+        nume(n), salariu(s), functie(f), departament(d) {
+    }
+
+    virtual void afisare() const = 0;
+    virtual double getSalariu() const { return salariu; }
+    virtual void setSalariu(double s) { salariu = s; }
+    virtual string getNume() const { return nume; }
+
+    void adaugaTask(shared_ptr<Task> task) {
+        taskuri.push_back(task);
+    }
+
+    void eliminaTask(int index) {
+        if (index >= 0 && index < taskuri.size())
+            taskuri.erase(taskuri.begin() + index);
+    }
+
+    void afisareTaskuri() const {
+        cout << "Taskuri pentru " << nume << ":\n";
+        for (auto& task : taskuri) task->afisare();
+    }
+
+    virtual ~Angajat() {}
+};
+
+class Muncitor : public Angajat {
+public:
+    Muncitor(string n, double s, string d)
+        : Angajat(n, s, "Muncitor", d) {
+    }
+
+    void afisare() const override {
+        cout << "[Muncitor] " << nume << ", Salariu: " << salariu
+            << ", Departament: " << departament << endl;
+        afisareTaskuri();
+    }
+};
+
+class Manager : public Angajat {
+    vector<shared_ptr<Angajat>> subordonati;
+
+public:
+    Manager(string n, double s, string f, string d)
+        : Angajat(n, s, f, d) {
+    }
+
+    void adaugaSubordonat(shared_ptr<Angajat> a) {
+        subordonati.push_back(a);
+    }
+
+    void afisare() const override {
+        cout << "[Manager - " << functie << "] " << nume << ", Salariu: " << salariu
+            << ", Departament: " << departament << endl;
+        afisareTaskuri();
+        cout << "Subordonati:\n";
+        for (auto& sub : subordonati)
+            sub->afisare();
+    }
+
+    vector<shared_ptr<Angajat>>& getSubordonati() { return subordonati; }
+};
+
+#endif
---
+++ Proiect_POO/Proiect_POO/Source.cpp
@@ -0,0 +1,100 @@
+﻿#include "Header.h"
+
+int main() {
+    vector<shared_ptr<Angajat>> angajati;
+
+    auto ceo = make_shared<Manager>("Popescu Andrei", 15000, "CEO", "Conducere");
+
+    auto sefIT = make_shared<Manager>("Ionescu Maria", 10000, "Sef Departament", "IT");
+
+    auto teamLead1 = make_shared<Manager>("Georgescu Radu", 7000, "Team Leader", "IT");
+    auto teamLead2 = make_shared<Manager>("Lazar Ana", 6800, "Team Leader", "IT");
+
+    auto muncitor1 = make_shared<Muncitor>("Dumitru Ion", 4000, "IT");
+    auto muncitor2 = make_shared<Muncitor>("Barbu Elena", 4200, "IT");
+
+    teamLead1->adaugaSubordonat(muncitor1);
+    teamLead2->adaugaSubordonat(muncitor2);
+    sefIT->adaugaSubordonat(teamLead1);
+    sefIT->adaugaSubordonat(teamLead2);
+    ceo->adaugaSubordonat(sefIT);
+
+    muncitor1->adaugaTask(make_shared<TaskCuTermen>("Reparare server", "2025-05-15"));
+    muncitor2->adaugaTask(make_shared<TaskCuBuget>("Achiziție software", 5000));
+    teamLead1->adaugaTask(make_shared<TaskComplet>("Raport echipă", 1000, "2025-05-20"));
+
+    angajati.push_back(ceo);
+
+    int opt;
+    do {
+        cout << "\n1. Afisare angajati\n";
+        cout << "2. Total salarii\n";
+        cout << "3. Adaugare angajat\n";
+        cout << "4. Modificare salariu\n";
+        cout << "5. Adaugare task\n";
+        cout << "6. Eliminare task\n";
+        cout << "0. Iesire\n";
+        cout << "Optiune: ";
+        cin >> opt;
+
+        if (opt == 1) {
+            for (auto& a : angajati)
+                a->afisare();
+        }
+        else if (opt == 2) {
+            double total = 0;
+            for (auto& a : angajati)
+                total += a->getSalariu();
+            cout << "Total salarii: " << total << endl;
+        }
+        else if (opt == 3) {
+            string nume, dept;
+            double salariu;
+            cout << "Nume: "; cin >> ws; getline(cin, nume);
+            cout << "Salariu: "; cin >> salariu;
+            cout << "Departament: "; cin >> dept;
+            angajati.push_back(make_shared<Muncitor>(nume, salariu, dept));
+            cout << "Angajat adaugat.\n";
+        }
+        else if (opt == 4) {
+            string nume;
+            double nou;
+            cout << "Nume angajat: "; cin >> ws; getline(cin, nume);
+            bool gasit = false;
+            for (auto& a : angajati) {
+                if (a->getNume() == nume) {
+                    cout << "Salariu nou: "; cin >> nou;
+                    a->setSalariu(nou);
+                    gasit = true;
+                    break;
+                }
+            }
+            if (!gasit) cout << "Angajatul nu a fost gasit.\n";
+        }
+        else if (opt == 5) {
+            string nume;
+            cout << "Nume angajat: "; cin >> ws; getline(cin, nume);
+            for (auto& a : angajati) {
+                if (a->getNume() == nume) {
+                    a->adaugaTask(make_shared<TaskCuTermen>("Nou Task", "2025-06-01"));
+                    cout << "Task adaugat.\n";
+                    break;
+                }
+            }
+        }
+        else if (opt == 6) {
+            string nume;
+            cout << "Nume angajat: "; cin >> ws; getline(cin, nume);
+            for (auto& a : angajati) {
+                if (a->getNume() == nume) {
+                    a->eliminaTask(0);
+                    cout << "Primul task eliminat (daca exista).\n";
+                    break;
+                }
+            }
+        }
+
+    } while (opt != 0);
+
+    return 0;
+}
